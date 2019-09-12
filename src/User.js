2e30218c39ed3b27836bd368ae84a0b1c91869c1
eric-klimowich <eklimowich@hotmail.com>
2019-09-12 13:53:29 -0400

    Attempting to fetch user profile after creation using token, currently unsuccessful

--- src/User.js
+++ src/User.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 
 class User extends Component {
 
+  state = {
+    jwt: ''
+  }
+
   componentDidMount() {
     fetch('http://localhost:3000/api/v1/users', {
       method: 'POST',
@@ -11,7 +15,7 @@ class User extends Component {
       },
       body: JSON.stringify({
         user: {
-          username: 'guy2',
+          username: 'guy14',
           password: 'hi',
           bio: 'King of Flavortown, USA',
           avatar: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Guy_Fieri_at_Guantanamo_2.jpg'
@@ -19,10 +23,23 @@ class User extends Component {
       })
     })
     .then(r => r.json())
-    .then(console.log)
+    .then(user => {
+      console.log(user)
+      this.setState({
+        jwt: user.jwt
+      }, () => {
+        fetch('http://localhost:3000/api/v1/profile', {
+                method: 'GET',
+                headers: {
+                  Authorization: this.state.jwt
+                }
+              })
+      })
+    })
   }
 
   render() {
+    console.log(this.state.jwt)
     return (
       <h1>In User</h1>
     )
