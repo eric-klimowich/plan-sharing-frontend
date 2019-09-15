185ca77df875186df67b6f1366625cd6e98d3c28
eric-klimowich <eklimowich@hotmail.com>
2019-09-15 07:43:30 -0400

    Created password, bio, and avatar fields for new user form

--- src/NewUser.js
+++ src/NewUser.js
@@ -4,11 +4,25 @@ const NewUser = () => {
   return (
     <div>
       <form>
-      <input
-        type="text"
-        name="username"
-        placeholder="enter username..."
-      />
+        <input
+          type="text"
+          name="username"
+          placeholder="enter username..."
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="enter password..."
+        />
+        <textarea
+          name="bio"
+          placeholder="enter bio..."
+        />
+        <input
+          type="text"
+          name="avatar"
+          placeholder="enter avatar..."
+        />
       </form>
     </div>
   )
