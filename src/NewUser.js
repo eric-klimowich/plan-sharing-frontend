c32994378d4c1b82bda8afb0609cd12ed87467fc
eric-klimowich <eklimowich@hotmail.com>
2019-09-15 12:00:13 -0400

    Converted NewUser from a functional component to a class component to be able to hold state for the form input

--- src/NewUser.js
+++ src/NewUser.js
@@ -1,31 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const NewUser = () => {
-  return (
-    <div>
-      <form>
-        <input
-          type="text"
-          name="username"
-          placeholder="enter username..."
-        />
-        <input
-          type="text"
-          name="password"
-          placeholder="enter password..."
-        />
-        <textarea
-          name="bio"
-          placeholder="enter bio..."
-        />
-        <input
-          type="text"
-          name="avatar"
-          placeholder="enter avatar..."
-        />
-      </form>
-    </div>
-  )
+class NewUser extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            name="username"
+            placeholder="enter username..."
+          />
+          <input
+            type="text"
+            name="password"
+            placeholder="enter password..."
+          />
+          <textarea
+            name="bio"
+            placeholder="enter bio..."
+          />
+          <input
+            type="text"
+            name="avatar"
+            placeholder="enter avatar..."
+          />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default NewUser
