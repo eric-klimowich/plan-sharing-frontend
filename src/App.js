75dc4adfe4c8cb9de7b2d97e3355dbc807375a84
eric-klimowich <eklimowich@hotmail.com>
2019-09-17 09:25:08 -0500

    Created new route for returning user to login, changed route for new user

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import User from './User'
 import Home from './Home'
 import Profile from './Profile'
 import NewUser from './NewUser'
+import ReturningUser from './ReturningUser'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Route path="/" exact component={Home} />
-        <Route path="/login" exact component={NewUser} />
+        <Route path="/login" exact component={ReturningUser} />
+        <Route path="/signup" exact component={NewUser} />
         <Route path="/profile" exact component={Profile} />
       </BrowserRouter>
     </div>
