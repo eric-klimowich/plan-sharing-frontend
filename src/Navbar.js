3ea1f0d59960cf4cb9bc9e3a888283974ca4a8a2
eric-klimowich <eklimowich@hotmail.com>
2019-09-18 10:30:28 -0500

    Removed initial CSS from App, set up classes for Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <header>
-      <div>
+    <header className="main-header">
+      <div className="main-header__title">
         <Link to="/">
           Plan Sharing
         </Link>
       </div>
-      <nav>
-        <ul>
-          <li>
+      <nav className="main-nav">
+        <ul className="main-nav__items">
+          <li className="main-nav__item">
             <Link to="/">
               Home
             </Link>
           </li>
-          <li>
+          <li className="main-nav__item">
             <Link to="/login">
               Login
             </Link>
           </li>
-          <li>
+          <li className="main-nav__item">
             <Link to="/signup">
               Sign up
             </Link>
