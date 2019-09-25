1652fbec0af1a6abfd8d1048c0c5cf0e87c3bf1b
eric-klimowich <eklimowich@hotmail.com>
2019-09-25 10:13:46 -0400

    Created form for adding a lesson

--- src/AddLesson.js
+++ src/AddLesson.js
@@ -3,7 +3,28 @@ import React, { Component } from 'react'
 class AddLesson extends Component {
   render() {
     return (
-      <div>In AddLesson</div>
+      <div className="add-lesson">
+        <h1 className="add-lesson__heading">Enter Lesson Info</h1>
+        <form className="add-lesson-form">
+          <input
+            className="add-lesson-form__input"
+            type="text"
+            name="title"
+            placeholder="enter title..."
+          />
+          <input
+            className="add-lesson-form__input"
+            type="text"
+            name="content"
+            placeholder="enter content..."
+          />
+          <input
+            type="submit"
+            name="submit"
+            value="Submit"
+          />
+        </form>
+      </div>
     )
   }
 }
