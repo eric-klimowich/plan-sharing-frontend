9f66372d57eab7c9b38aefd9e9a6a89f596cb517
eric-klimowich <eklimowich@hotmail.com>
2019-09-28 10:05:23 -0400

    Added fetch with sample token to attempt logging lessons

--- src/LessonsContainer.js
+++ src/LessonsContainer.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
 
 class LessonsContainer extends Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/lessons', {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.O0la-F93yrPjznLJJGn2wNIVKhQLR3YQrIimZaf211o'
+      }
+    })
+    .then(r => r.json())
+    .then(console.log)
+  }
+
   render() {
     return (
       <div>In LessonsContainer</div>
