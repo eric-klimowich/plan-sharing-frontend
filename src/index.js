743c3cab1dbcedc266b93382e70efa99f310dacf
eric-klimowich <eklimowich@hotmail.com>
2019-09-30 15:11:48 -0400

    Created store and added Provider to index.js

--- src/index.js
+++ src/index.js
@@ -9,8 +9,13 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import reducer from './reducers'
 
+const store = createStore(reducer)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
