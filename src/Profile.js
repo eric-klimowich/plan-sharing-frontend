b75a3796d2f047b88f4ab767b26c8b40c91c07e1
eric-klimowich <eklimowich@hotmail.com>
2019-10-01 16:30:19 -0400

    Created method to render user information, inserted into conditional rendering based on user login

--- src/Profile.js
+++ src/Profile.js
@@ -2,11 +2,21 @@ import React, { Fragment, Component } from 'react'
 import { connect } from 'react-redux'
 
 class Profile extends Component {
+
+  renderUserInfo = () => {
+    return (
+      <Fragment>
+        <h1>{this.props.user.user.username}</h1>
+        <p>{this.props.user.user.bio}</p>
+      </Fragment>
+    )
+  }
+
   render() {
     console.log(this.props)
     return (
       <Fragment>
-        {this.props.user ? <h1>{this.props.user.user.username}</h1> : <h1>Please Login</h1>}
+        {this.props.user ? this.renderUserInfo() : <h1>Please Login</h1>}
       </Fragment>
     )
   }
