4272fe54257f81aacd0f9942e96605a63e453809
eric-klimowich <eklimowich@hotmail.com>
2019-10-01 15:31:56 -0400

    Connected Profile component to store and rendered username if user exists

--- src/Profile.js
+++ src/Profile.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { Fragment, Component } from 'react'
+import { connect } from 'react-redux'
 
-const Profile = () => {
-  return (
-    <div>In Profile</div>
-  )
+class Profile extends Component {
+  render() {
+    console.log(this.props)
+    return (
+      <Fragment>
+        {this.props.user ? <h1>{this.props.user.user.username}</h1> : <h1>Please Login</h1>}
+      </Fragment>
+    )
+  }
 }
 
-export default Profile
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(Profile)
