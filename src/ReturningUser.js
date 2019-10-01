7714b6b6f49ec28cd0939a672b70e852c3e1f1ce
eric-klimowich <eklimowich@hotmail.com>
2019-10-01 16:42:30 -0400

    Conditionally redirecting to Profile page from Login if user is not null

--- src/ReturningUser.js
+++ src/ReturningUser.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Redirect} from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { setUser } from './actions'
@@ -29,6 +30,10 @@ class ReturningUser extends Component {
     .then(user => {
       this.props.setUser(user)
     })
+    this.setState({
+      username: '',
+      password: ''
+    })
   }
 
   handleChangeUserInput = event => {
@@ -40,34 +45,39 @@ class ReturningUser extends Component {
   render() {
     console.log(this.state)
     console.log(this.props)
-    return (
-      <div className="login">
-        <h1 className="login__heading">Please enter to login</h1>
-        <form className="login-form" onSubmit={(event) => this.handleLoginUser(event, this.state)}>
-          <input
-            className="login-form__input"
-            type="text"
-            name="username"
-            value={this.state.username}
-            placeholder="enter username..."
-            onChange={this.handleChangeUserInput}
-          />
-          <input
-            className="login-form__input"
-            type="text"
-            name="password"
-            value={this.state.password}
-            placeholder="enter password..."
-            onChange={this.handleChangeUserInput}
-          />
-          <input
-            type="submit"
-            name="submit"
-            value="Submit"
-          />
-        </form>
-      </div>
-    )
+    if (!this.props.user) {
+      return (
+        <div className="login">
+          <h1 className="login__heading">Please enter to login</h1>
+          <form className="login-form" onSubmit={(event) => this.handleLoginUser(event, this.state)}>
+            <input
+              className="login-form__input"
+              type="text"
+              name="username"
+              value={this.state.username}
+              placeholder="enter username..."
+              onChange={this.handleChangeUserInput}
+            />
+            <input
+              className="login-form__input"
+              type="text"
+              name="password"
+              value={this.state.password}
+              placeholder="enter password..."
+              onChange={this.handleChangeUserInput}
+            />
+            <input
+              type="submit"
+              name="submit"
+              value="Submit"
+            />
+          </form>
+        </div>
+      )
+    } else {
+      return <Redirect to="/profile" />
+    }
+
   }
 }
 
