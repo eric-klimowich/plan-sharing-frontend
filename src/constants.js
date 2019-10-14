87f1aba9269cf2fac5537cbdccaf6775d0f62711
eric-klimowich <eklimowich@hotmail.com>
2019-10-14 18:06:56 -0400

    Created file for constants, imported grades and subjects into AddLesson component

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const grades = ["K", "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th"]
+
+export const subjects = ["ELA", "Math", "Science", "Social Studies", "Art", "Music", "PE"]
--- src/pages/AddLesson.js
+++ src/pages/AddLesson.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import { renderAddLessonForm } from '../forms'
-
-const grades = ["K", "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th"]
-const subjects = ["ELA", "Math", "Science", "Social Studies", "Art", "Music", "PE"]
+import { grades } from '../constants'
+import { subjects } from '../constants'
 
 class AddLesson extends Component {
 
