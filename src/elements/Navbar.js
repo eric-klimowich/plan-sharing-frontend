24f6b4eb887df5b72c3d3984e098f2596e6dba98
eric-klimowich <eklimowich@hotmail.com>
2019-10-16 12:04:20 -0400

    Conditionally rendering Navbar links based on jwt, runs with refresh only, need to fix

--- src/elements/Navbar.js
+++ src/elements/Navbar.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
+
+import Adapter from '../Adapter'
 
 const Navbar = () => {
   return (
@@ -9,35 +11,62 @@ const Navbar = () => {
           Plan Sharing
         </Link>
       </div>
-      <nav className="main-nav">
-        <ul className="main-nav__items">
-          <li className="main-nav__item">
-            <Link to="/profile">
-              Profile
-            </Link>
-          </li>
-          <li className="main-nav__item">
-            <Link to="/lessons">
-              All Lessons
-            </Link>
-          </li>
-          <li className="main-nav__item">
-            <Link to="/profile/addlesson">
-              Add Lesson
-            </Link>
-          </li>
-          <li className="main-nav__item">
-            <Link to="/login">
-              Login
-            </Link>
-          </li>
-          <li className="main-nav__item">
-            <Link to="/signup">
-              Sign up
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      {
+        !!Adapter.getToken() ?
+          <Fragment>
+            <nav className="main-nav">
+              <ul className="main-nav__items">
+                <li className="main-nav__item">
+                  <Link to="/profile">
+                    Profile
+                  </Link>
+                </li>
+                <li className="main-nav__item">
+                  <Link to="/lessons">
+                    All Lessons
+                  </Link>
+                </li>
+                <li className="main-nav__item">
+                  <Link to="/profile/addlesson">
+                    Add Lesson
+                  </Link>
+                </li>
+                <li className="main-nav__item">
+                  <Link to="/login">
+                    Login
+                  </Link>
+                </li>
+                <li className="main-nav__item">
+                  <Link to="/signup">
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </Fragment>
+          :
+          <Fragment>
+            <nav className="main-nav">
+              <ul className="main-nav__items">
+                <li className="main-nav__item">
+                  <Link to="/lessons">
+                    All Lessons
+                  </Link>
+                </li>
+                <li className="main-nav__item">
+                  <Link to="/login">
+                    Login
+                  </Link>
+                </li>
+                <li className="main-nav__item">
+                  <Link to="/signup">
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </Fragment>
+      }
     </header>
   )
 }
