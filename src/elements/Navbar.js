acd7ec441db9a475d0e33810ae2af1f90564a703
eric-klimowich <eklimowich@hotmail.com>
2019-10-16 14:17:29 -0400

    Implemented simple logout method in Navbar to delete token

--- src/elements/Navbar.js
+++ src/elements/Navbar.js
@@ -1,74 +1,73 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 import Adapter from '../Adapter'
 
-const Navbar = () => {
-  return (
-    <header className="main-header">
-      <div className="main-header__title">
-        <Link to="/">
-          Plan Sharing
-        </Link>
-      </div>
-      {
-        !!Adapter.getToken() ?
-          <Fragment>
-            <nav className="main-nav">
-              <ul className="main-nav__items">
-                <li className="main-nav__item">
-                  <Link to="/profile">
-                    Profile
-                  </Link>
-                </li>
-                <li className="main-nav__item">
-                  <Link to="/lessons">
-                    All Lessons
-                  </Link>
-                </li>
-                <li className="main-nav__item">
-                  <Link to="/profile/addlesson">
-                    Add Lesson
-                  </Link>
-                </li>
-                <li className="main-nav__item">
-                  <Link to="/login">
-                    Login
-                  </Link>
-                </li>
-                <li className="main-nav__item">
-                  <Link to="/signup">
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </Fragment>
-          :
-          <Fragment>
-            <nav className="main-nav">
-              <ul className="main-nav__items">
-                <li className="main-nav__item">
-                  <Link to="/lessons">
-                    All Lessons
-                  </Link>
-                </li>
-                <li className="main-nav__item">
-                  <Link to="/login">
-                    Login
-                  </Link>
-                </li>
-                <li className="main-nav__item">
-                  <Link to="/signup">
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </Fragment>
-      }
-    </header>
-  )
+class Navbar extends Component {
+  handleLogout = () => {
+    localStorage.removeItem('token')
+  }
+
+  render() {
+    return (
+      <header className="main-header">
+        <div className="main-header__title">
+          <Link to="/">
+            Plan Sharing
+          </Link>
+        </div>
+        {
+          !!Adapter.getToken() ?
+            <Fragment>
+              <nav className="main-nav">
+                <ul className="main-nav__items">
+                  <li className="main-nav__item">
+                    <Link to="/profile">
+                      Profile
+                    </Link>
+                  </li>
+                  <li className="main-nav__item">
+                    <Link to="/lessons">
+                      All Lessons
+                    </Link>
+                  </li>
+                  <li className="main-nav__item">
+                    <Link to="/profile/addlesson">
+                      Add Lesson
+                    </Link>
+                  </li>
+                </ul>
+                <button onClick={this.handleLogout}>
+                  Logout
+                </button>
+              </nav>
+            </Fragment>
+            :
+            <Fragment>
+              <nav className="main-nav">
+                <ul className="main-nav__items">
+                  <li className="main-nav__item">
+                    <Link to="/lessons">
+                      All Lessons
+                    </Link>
+                  </li>
+                  <li className="main-nav__item">
+                    <Link to="/login">
+                      Login
+                    </Link>
+                  </li>
+                  <li className="main-nav__item">
+                    <Link to="/signup">
+                      Sign up
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </Fragment>
+        }
+      </header>
+    )
+  }
 }
 
 export default Navbar
