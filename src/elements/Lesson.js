a52319d8a30135202ac729735cdd5a4b1fc0f9ab
eric-klimowich <eklimowich@hotmail.com>
2019-10-23 09:24:16 -0400

    Converted Lesson component to a class component from functional

--- src/elements/Lesson.js
+++ src/elements/Lesson.js
@@ -1,20 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 
-const Lesson = props => {
-  console.log(props)
-  return (
-    <Fragment>
-      <h1>{props.title}</h1>
-      <p>{props.description}</p>
-      <p>Grade: {props.grade}</p>
-      <p>Subject: {props.subject}</p>
-      <p>Created by: {props.user}</p>
-      <a href={`http://localhost:3000/api/v1/lessons/${props.id}`}>
-        {props.fileName}
-      </a>
-      <button>Delete</button>
-    </Fragment>
-  )
+class Lesson extends Component {
+  render() {
+    console.log(this.props)
+    return (
+      <Fragment>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.description}</p>
+        <p>Grade: {this.props.grade}</p>
+        <p>Subject: {this.props.subject}</p>
+        <p>Created by: {this.props.user}</p>
+        <a href={`http://localhost:3000/api/v1/lessons/${this.props.id}`}>
+          {this.props.fileName}
+        </a>
+        <button>Delete</button>
+      </Fragment>
+    )
+  }
 }
 
 export default Lesson
