60dfe83a3c0a351b80f2e4b8d9e703365c4e51ac
eric-klimowich <eklimowich@hotmail.com>
2019-10-29 16:07:34 -0400

    Changed view of About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,11 +6,11 @@ const About = (props) => {
       <h1>
         Welcome to Teachers Helping Teachers!
       </h1>
-      <h1>
+      <h2>
         A way for all teachers, new and experienced, to share and find lesson plan and worksheet samples for free.
         Since we are not only teachers but also lifelong students, helpful feedback is always welcome.
         I hope this serves as a valuable resource to the teaching community.
-      </h1>
+      </h2>
     </Fragment>
   )
 }
