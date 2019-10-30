9f8ff5ae143d6bc1f2c6d54a4b5dbf259e8826fe
eric-klimowich <eklimowich@hotmail.com>
2019-10-30 13:02:16 -0400

    Created method to conditionally render input field to replace file

--- src/pages/EditLesson.js
+++ src/pages/EditLesson.js
@@ -52,6 +52,23 @@ class EditLesson extends Component {
     this.props.hideEditLessonForm()
   }
 
+  renderFileInputField = () => {
+    if () {
+      return (
+        <input
+          className="add-lesson-form__input"
+          type="file"
+          name="file"
+          placeholder="add lesson plan file...."
+          onChange={this.handleChangeLessonInput}
+        />
+      )
+    }
+    return (
+      <p>{this.props.pickedLesson.file_name}</p>
+    )
+  }
+
   render() {
     console.log(this.props)
     console.log(this.state)
@@ -60,8 +77,60 @@ class EditLesson extends Component {
     }
     return (
       <Fragment>
-      {renderLessonForm(this.handleEditLesson, this.state, this.handleChangeLessonInput, grades, subjects)}
-      <button onClick={this.handleCancelEditLesson}>Cancel</button>
+        <div className="add-lesson">
+          <h1 className="add-lesson__heading">Enter Lesson Info</h1>
+          <form className="add-lesson-form" onSubmit={this.handleEditLesson}>
+            <input
+              className="add-lesson-form__input"
+              type="text"
+              name="title"
+              value={this.state.title}
+              placeholder="enter title..."
+              onChange={this.handleChangeLessonInput}
+            />
+            <input
+              className="add-lesson-form__input"
+              type="text"
+              name="description"
+              value={this.state.description}
+              placeholder="enter description..."
+              onChange={this.handleChangeLessonInput}
+            />
+            <select
+              className="add-lesson-form__input"
+              name="grade"
+              value={this.state.grade}
+              onChange={this.handleChangeLessonInput}
+            >
+              <option>Grade</option>
+                {grades.map(grade => {
+                  return (
+                    <option key={grade} value={grade} >{grade}</option>
+                  )})
+                }
+            </select>
+            <select
+              className="add-lesson-form__input"
+              name="subject"
+              value={this.state.subject}
+              onChange={this.handleChangeLessonInput}
+            >
+              <option>Subject</option>
+                {subjects.map(subject => {
+                  return (
+                    <option key={subject} value={subject} >{subject}</option>
+                  )})
+                }
+            </select>
+
+            <input
+              type="submit"
+              name="submit"
+              value="Submit"
+            />
+          </form>
+        </div>
+        <button onClick={this.handleCancelEditLesson}>Cancel</button>
       </Fragment>
     )
   }
