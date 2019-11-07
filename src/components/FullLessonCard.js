03a47620004b5263005f0d5c02239d11a91ed12c
eric-klimowich <eklimowich@hotmail.com>
2019-11-07 11:14:26 -0500

    Conditionally rendered add comment form based on user logged in

--- src/components/FullLessonCard.js
+++ src/components/FullLessonCard.js
@@ -56,7 +56,7 @@ class FullLessonCard extends Component {
         <br/>
         {this.renderEditAndDeleteButtons()}
         <button onClick={this.handleBackToAllLessons}>Back to All Lessons</button>
-        <AddComment lessonId={this.props.pickedLesson.id} userId={this.props.user.id}/>
+        {!!this.props.user ? <AddComment lessonId={this.props.pickedLesson.id} userId={this.props.user.id}/> : null}
         <CommentsContainer comments={this.props.pickedLesson.comments}/>
       </div>
     )
