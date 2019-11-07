b9ac6a1133c280e9f755974bfce74e1efb6a94c3
eric-klimowich <eklimowich@hotmail.com>
2019-11-07 10:45:10 -0500

    Created local state for add comment component

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 
 class AddComment extends Component {
+
+  state = {
+    commentInput: ''
+  }
+
   render() {
+    console.log(this.state)
     return (
       <form>
         What did you think of this lesson? Leave a comment here:
         <textarea
           name="commentInput"
+          value={this.state.commentInput}
         />
         <input
           type="submit"
