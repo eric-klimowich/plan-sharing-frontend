e69c7a4014b79aeadbfa0505bcc5a5fbcc6caf04
eric-klimowich <eklimowich@hotmail.com>
2019-11-07 12:05:57 -0500

    Iterated through comments in comments container to pass comment down to comment component

---
+++ src/components/Comment.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Comment = props => {
+  return (
+    <div>In Comment</div>
+  )
+}
+
+export default Comment
--- src/containers/CommentsContainer.js
+++ src/containers/CommentsContainer.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Comment from '../components/Comment'
 
 const CommentsContainer = props => {
-  console.log(props)
+  console.log(props.comments)
   return (
-    <div>In CommentsContainer</div>
+    <Fragment>
+      {props.comments.map(comment => {
+        return (
+          <Comment
+            key={comment.id}
+            comment={comment}
+          />
+        )
+      })}
+    </Fragment>
   )
 }
 
