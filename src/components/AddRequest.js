593cf37185faa710d3130cea89d2eab5bac49655
eric-klimowich <eklimowich@hotmail.com>
2019-11-12 12:43:54 -0500

    Created initial form to add request

--- src/components/AddRequest.js
+++ src/components/AddRequest.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 const AddRequest = () => {
   return (
-    <div>In AddRequest</div>
+    <div className="add-request">
+      <h1 className="add-request__heading">Enter Request</h1>
+      <form className="add-request-form">
+        <input
+          className="add-request-form__input"
+          type="text"
+          name="title"
+          placeholder="enter title..."
+        />
+        <input
+          className="add-request-form__input"
+          type="text"
+          name="description"
+          placeholder="enter description..."
+        />
+        <input
+          type="submit"
+          name="submit"
+          value="Submit"
+        />
+      </form>
+    </div>
   )
 }
 
