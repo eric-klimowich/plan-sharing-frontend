ebfa9aeb97586a0620a73eb301200591c836a6a1
eric-klimowich <eklimowich@hotmail.com>
2019-11-12 12:50:00 -0500

    Converted AddRequest to a class component

--- src/components/AddRequest.js
+++ src/components/AddRequest.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const AddRequest = () => {
-  return (
-    <div className="add-request">
-      <h1 className="add-request__heading">Enter Request</h1>
-      <form className="add-request-form">
-        <input
-          className="add-request-form__input"
-          type="text"
-          name="title"
-          placeholder="enter title..."
-        />
-        <input
-          className="add-request-form__input"
-          type="text"
-          name="description"
-          placeholder="enter description..."
-        />
-        <input
-          type="submit"
-          name="submit"
-          value="Submit"
-        />
-      </form>
-    </div>
-  )
+class AddRequest extends Component {
+  render() {
+    return (
+      <div className="add-request">
+        <h1 className="add-request__heading">Enter Request</h1>
+        <form className="add-request-form">
+          <input
+            className="add-request-form__input"
+            type="text"
+            name="title"
+            placeholder="enter title..."
+          />
+          <input
+            className="add-request-form__input"
+            type="text"
+            name="description"
+            placeholder="enter description..."
+          />
+          <input
+            type="submit"
+            name="submit"
+            value="Submit"
+          />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default AddRequest
