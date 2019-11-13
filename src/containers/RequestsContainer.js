70125f2b577412eda3f113c713c7d94af5be4419
eric-klimowich <eklimowich@hotmail.com>
2019-11-13 10:49:01 -0500

    Connected Request to RequestsContainers

--- src/containers/RequestsContainer.js
+++ src/containers/RequestsContainer.js
@@ -1,18 +1,31 @@
 import React, { Component, Fragment } from 'react'
 import Adapter from '../Adapter'
+import Request from '../components/Request'
 import AddRequest from '../components/AddRequest'
 
 class RequestsContainer extends Component {
 
+  state = {
+    requests: []
+  }
+
   componentDidMount() {
     Adapter.getRequests()
-      .then(console.log)
+      .then(requests => this.setState({ requests }))
   }
 
   render() {
+    console.log(this.state)
     return (
       <Fragment>
-        <div>In RequestsContainer</div>
+        {this.state.requests.map(request => {
+          return (
+            <Request
+              key={request.id}
+              request={request}
+            />
+          )
+        })}
         <AddRequest />
       </Fragment>
     )
