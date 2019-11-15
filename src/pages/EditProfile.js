a7017fc750e0fc75d4097e460dfd51e82ce82ef8
eric-klimowich <eklimowich@hotmail.com>
2019-11-15 11:40:23 -0500

    Converted EditProfile to class component

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,12 +1,14 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 
-const EditProfile = () => {
-  return (
-    <Fragment>
-      <div>In EditProfile</div>
-      <button>Back to Profile</button>
-    </Fragment>
-  )
+class EditProfile extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>In EditProfile</div>
+        <button>Back to Profile</button>
+      </Fragment>
+    )
+  }
 }
 
 export default EditProfile
