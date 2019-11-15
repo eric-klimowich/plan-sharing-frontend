8a2d8ad82337a46f983c9a15222c63e40c05abac
eric-klimowich <eklimowich@hotmail.com>
2019-11-15 11:45:28 -0500

    Connected EditProfile to store, imported action to hide user profile edit form

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,4 +1,7 @@
 import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { hideEditProfileForm } from '../actions'
+
 
 class EditProfile extends Component {
   render() {
@@ -11,4 +14,16 @@ class EditProfile extends Component {
   }
 }
 
-export default EditProfile
+const mapStateToProps = state => {
+  return {
+  editProfile: state.editProfile
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    hideEditProfileForm: () => dispatch(hideEditProfileForm())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditProfile)
